c78156a4e5f302718b62fa7d52d97cdb441d5a6f
Sachin Sharma - +919034283082 <140776112+SachinSharma48@users.noreply.github.com>
2023-08-06 12:41:51 +0530

    Create Scraping_Email_.py

---
+++ Scraping_Email_.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def get_email_addresses(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    email_addresses = set()
+
+    # Find email addresses using regex
+    email_pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+    for tag in soup.find_all('a'):
+        if 'mailto:' in tag.get('href', ''):
+            email = tag.get('href').split('mailto:')[1]
+            if email_pattern.match(email):
+                email_addresses.add(email)
+
+    return list(email_addresses)
+
+sachin_df = pd.read_csv('C:/Users/Sachin_Sharma/EMail.csv')
+websites = sachin_df['Website'].tolist()
+
+rahul_df = pd.DataFrame(columns=['Website', 'Email_ID'])
+
+for website in websites:
+    try:
+        # Fetch email addresses from the front page of the website
+        email_addresses = get_email_addresses(website)
+
+        # Append data to rahul_df DataFrame
+        if email_addresses:
+            rahul_df = rahul_df.append({'Website': website, 'Email_ID': ', '.join(email_addresses)}, ignore_index=True)
+        else:
+            rahul_df = rahul_df.append({'Website': website, 'Email_ID': 'No email addresses found'}, ignore_index=True)
+
+        # Save the data to Rahul.csv after each iteration
+        rahul_df.to_csv('C:/Users/Desktop/sachin.csv', index=False)
+
+        print(f"Fetched email addresses from {website}")
+    except Exception as e:
+        print(f"Error fetching email addresses from {website}: {e}")
+
+print("Done)
